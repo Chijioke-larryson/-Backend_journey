3b78344f857a562898299d0d5c7e24edfd4624c3
SilentXBT <larrysongift@gmail.com>
2025-10-16 16:41:38 -0700

    error handling

---
+++ chapter_4/src/routes/todoRoutes.js
@@ -0,0 +1,51 @@
+import express from "express";
+import db from "../db.js";
+
+
+
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+        const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+        const todos = getTodos.all(req.userId)
+        res.json(todos)
+
+})
+
+
+    router.post('/', (req, res) => {
+        const {task} = req.body;
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+       const result = insertTodo.run(req.userId, task)
+
+        res.json({id: result.lastInsertRowid, task, completed: 0 })
+    })
+
+    router.put('/:id', (req, res) => {
+
+        console.log('Request body:', req.body);
+
+        const { completed} = req.body;
+        const {id} = req.params;
+        const {page} = req.query;
+
+        const updatedTodo = db.prepare('UPDATE Todos SET completed = ?  WHERE  id = ? ')
+        updatedTodo.run(completed, id)
+        res.json({message: 'Todo Completed'})
+    })
+
+
+    router.delete('/:id', (req, res) => {
+        const  { id } = req.param
+        const userId = req.userId
+        const deleteTodo = db.prepare( 'DELETE FROM todos WHERE id = ?  AND user_id = ? ')
+
+        deleteTodo.run(id, userId)
+        res.send({message:"Todo Deleted"})
+
+
+    })
+
+
+    export default router;
---
+++ chapter_4/src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import path, {dirname} from "path";
+import {fileURLToPath} from 'url'
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const  app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
+
+
+})
+
+
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes )
+
+
+
+app.listen(PORT, () => {
+    console.log(`server has started on port:${PORT}`)
+})
+
