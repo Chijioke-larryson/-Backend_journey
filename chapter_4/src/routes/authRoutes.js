cfe5fd81105ab327716e03e34caedc07a60a1129
SilentXBT <larrysongift@gmail.com>
2025-10-19 17:31:41 -0700

    Dockerization of the projectr

--- chapter_4/src/routes/authRoutes.js
+++ chapter_4/src/routes/authRoutes.js
@@ -1,34 +1,41 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import db from '../db.js'
+
+import prisma from "../prismaClient.js";
 
 
 const router = express.Router()
 
 
 
-router.post('/register' , (req, res) => {
+router.post('/register' ,  async (req, res) => {
     const {username, password} = req.body
 
 
     const hashedPassword =  bcrypt.hashSync(password, 8)
 
     try {
-        const insertUser = db.prepare(`INSERT INTO users (username, password)
-VALUES  (?, ?)`)
-
-        const result  = insertUser.run(username, hashedPassword)
+        const user = await prisma.user.create({
+            date: {
+                username,
+                password: hashedPassword
+            }
+        })
 
 
 
         const defaultTodo = 'Hello :) Add your First Todo!'
-        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
-        VALUES (?, ?)`)
-        insertTodo.run(result.lastInsertRowid, defaultTodo)
+        await prisma.todo.create({
+            date: {
+                task: defaultTodo,
+                userId: user.id
+            }
+        })
 
 
-        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, { expiresIn: '24h'})
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, { expiresIn: '24h'})
         res.json({ token})
 
 
@@ -47,13 +54,16 @@ VALUES  (?, ?)`)
 
 
 
-router.post('/login', (req, res) => {
+router.post('/login',  async (req, res) => {
     const {username, password} = req.body
 
     try{
 
-        const getUser = db.prepare(`SELECT * FROM users WHERE  username = ? `)
-        const user = getUser.get(username)
+        const user = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        })
 
 
         if (!user) {return res.status(404).send({message: 'user not found'})}
